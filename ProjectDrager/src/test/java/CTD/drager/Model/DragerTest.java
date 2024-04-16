0d29d53e20f83f77bd4b10d22226c667660e3939
Neskj <pronin.qq@gmail.com>
2024-04-16 07:18:43 +0300

    add dragerSettersTest

--- ProjectDrager/src/test/java/CTD/drager/Model/DragerTest.java
+++ ProjectDrager/src/test/java/CTD/drager/Model/DragerTest.java
@@ -10,14 +10,35 @@ public class DragerTest {
     @Test
     public void createDragerTest(){
 
-        Drager drager =new Drager("Drager x-Am 1000","ARXX-0001","0123456","Aleksandr Pushkin","31.12.2054","Calibration");
+        Drager drager = new Drager("Drager x-Am 1000", "ARXX-0001", "0123456", "Aleksandr Pushkin", "31.12.2054", "Calibration");
         assertNotNull(drager);
-        assertEquals("Drager x-Am 1000",drager.getModel());
-        assertEquals("ARXX-0001",drager.getSerial());
-        assertEquals("0123456",drager.getInventoryNumber());
-        assertEquals("Aleksandr Pushkin",drager.getOwner());
-        assertEquals("31.12.2054",drager.getVerification());
-        assertEquals("Calibration",drager.getStatus());
-        assertEquals("white",drager.getFlag());
+        assertEquals("Drager x-Am 1000", drager.getModel());
+        assertEquals("ARXX-0001", drager.getSerial());
+        assertEquals("0123456", drager.getInventoryNumber());
+        assertEquals("Aleksandr Pushkin", drager.getOwner());
+        assertEquals("31.12.2054", drager.getVerification());
+        assertEquals("Calibration", drager.getStatus());
+        assertEquals("white", drager.getFlag());
+    }
+
+    @Test
+    public void dragerSettersTest() {
+
+        Drager drager = new Drager();
+        drager.setModel("Drager x-Am 1000");
+        drager.setSerial("ARXX-0001");
+        drager.setInventoryNumber("0123456");
+        drager.setOwner("Aleksandr Pushkin");
+        drager.setVerification("31.12.2054");
+        drager.setStatus("Calibration");
+        drager.setFlag("red");
+        assertNotNull(drager);
+        assertEquals("Drager x-Am 1000", drager.getModel());
+        assertEquals("ARXX-0001", drager.getSerial());
+        assertEquals("0123456", drager.getInventoryNumber());
+        assertEquals("Aleksandr Pushkin", drager.getOwner());
+        assertEquals("31.12.2054", drager.getVerification());
+        assertEquals("Calibration", drager.getStatus());
+        assertEquals("red", drager.getFlag());
     }
 }
